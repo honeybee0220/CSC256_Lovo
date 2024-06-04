6381a3d85f4bc0062bac5486724c4fefd2fa3dd1
honeybee0220 <144168513+honeybee0220@users.noreply.github.com>
2024-06-04 12:21:03 -0500

    Add files via upload

---
+++ week4script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n - 1);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n >= slides.length) {slideIndex = 0}    
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";  
+    dots[slideIndex].className += " active";
+}
